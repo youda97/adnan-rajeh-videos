254c63daaea9027c7e37ce675d0d35e53c39a5ad
Yousef Ouda <ouda.yousef@ibm.com>
2022-10-30 23:51:19 -0400

    Add topic component

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import recentVideoSliceReducer from "./features/recentVideoSlice";
+import playlistSliceReducer from "./features/playlistSlice";
+import playlistItemSliceReducer from "./features/playlistItemSlice";
 import videoSliceReducer from "./features/videoSlice";
 
 export default configureStore({
     reducer: {
+        recentVideo: recentVideoSliceReducer,
+        playlist: playlistSliceReducer,
+        playlistItem: playlistItemSliceReducer,
         video: videoSliceReducer,
     },
 });
