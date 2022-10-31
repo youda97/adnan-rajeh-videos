5214bd073ec991dab2ae92cb7a084057254e7675
Yousef Ouda <ouda.yousef@ibm.com>
2022-10-30 23:52:31 -0400

    Add single video slice

--- src/redux/features/videoSlice.ts
+++ src/redux/features/videoSlice.ts
@@ -1,22 +1,18 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const YOUTUBE_API = "https://www.googleapis.com/youtube/v3/search";
-const RESULTS = 4;
+const YOUTUBE_API = "https://www.googleapis.com/youtube/v3/videos";
 
 const initialState = {
     loading: false,
-    videos: [],
+    video: [],
     error: "",
 };
 
-export const fetchVideos = createAsyncThunk("video/fetchVideos", () => {
-    console.log(
-        `${YOUTUBE_API}?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&channelId=UCV4_IPy-P9Gbdvxn-FygEOg`
-    );
+export const fetchVideo = createAsyncThunk("video/fetchVideo", (video: any) => {
     return axios
         .get(
-            `${YOUTUBE_API}?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&channelId=UCV4_IPy-P9Gbdvxn-FygEOg&part=snippet,id&order=date&maxResults=${RESULTS}`
+            `${YOUTUBE_API}?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&id=${video}&part=snippet,contentDetails,statistics,status`
         )
         .then((response) => response.data);
 });
@@ -27,17 +23,17 @@ const videoSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(fetchVideos.pending, (state) => {
+            .addCase(fetchVideo.pending, (state) => {
                 state.loading = true;
             })
-            .addCase(fetchVideos.fulfilled, (state, action) => {
+            .addCase(fetchVideo.fulfilled, (state, action) => {
                 state.loading = false;
-                state.videos = action.payload;
+                state.video = action.payload;
                 state.error = "";
             })
-            .addCase(fetchVideos.rejected, (state, action) => {
+            .addCase(fetchVideo.rejected, (state, action) => {
                 state.loading = false;
-                state.videos = [];
+                state.video = [];
                 state.error = action.error.message || "Error fetching data.";
             });
     },
