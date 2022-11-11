0b7736ef9ad113ea2dbc45268ccc6fab6c84f739
Yousef Ouda <ouda.yousef@ibm.com>
2022-11-11 01:23:47 -0500

    Add scroll to top component

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Header } from "./components/Header";
 import { Topic } from "./components/Topic";
+import ScrollToTop from "./scrollToTop";
+import { Livestream } from "./components/Livestream";
 
 function App() {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Routes>
                 <Route path="/" element={<Header />}>
                     <Route index element={<Home />} />
                     <Route path="topics/:id" element={<Topic />} />
-                    {/* <Route path="/livestream" element={< />} />
-                        <Route path="/settings" element={< />} />
+                    <Route path="/livestream" element={<Livestream />} />
+                    {/* <Route path="/settings" element={< />} />
                         <Route path="/help" element={< />} /> */}
                 </Route>
             </Routes>
---
+++ src/components/Livestream.tsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+
+interface Props {}
+
+export const Livestream = ({}: Props) => {
+    return <div />;
+};
