ac29f1cb416a9b7a17959f9c6f859d2ac0eddf26
Yousef Ouda <ouda.yousef@ibm.com>
2022-11-11 14:42:11 -0500

    Add livestream component

--- src/components/Livestream.tsx
+++ src/components/Livestream.tsx
@@ -1,8 +1,42 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { parse } from "node-html-parser";
+import fetch from "node-fetch";
 
 interface Props {}
 
+const channelID = "UCV4_IPy-P9Gbdvxn-FygEOg";
+
 export const Livestream = ({}: Props) => {
-    return <div />;
+    const [isStreaming, setIsStreaming] = useState(false);
+
+    const getLiveStream = async () => {
+        const response = await fetch(
+            `https://youtube.com/channel/${channelID}/live`,
+            { mode: "no-cors" }
+        );
+        const text = await response.text();
+        const html = parse(text);
+
+        const canonicalURLTag = html.querySelector("link[rel=canonical]");
+
+        if (canonicalURLTag) {
+            const canonicalURL = canonicalURLTag.getAttribute("href");
+            const isChannelStreaming = canonicalURL.includes("/watch?v=");
+
+            setIsStreaming(isChannelStreaming);
+        } else {
+            setIsStreaming(false);
+        }
+    };
+
+    useEffect(() => {
+        getLiveStream();
+    }, []);
+
+    return (
+        <div className="container livestream">
+            {isStreaming ? "Coming soon..." : "No live videos"}
+        </div>
+    );
 };
